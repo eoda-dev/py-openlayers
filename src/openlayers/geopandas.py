340873a48a2ff4fb1d206116f3fb13063739ce71
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-04-25 16:43:17 +0200

    Refactor projection

--- src/openlayers/geopandas.py
+++ src/openlayers/geopandas.py
@@ -1,8 +1,9 @@
 import geopandas as gpd
 
-CRS = "EPSG:3857"
+from .view import CRS
 
-def gdf_to_geojson(data: gpd.GeoDataFrame, crs: str | None = CRS):
+
+def gdf_to_geojson(data: gpd.GeoDataFrame, crs: str | None = CRS.MERCATOR):
     if crs:
         data = data.to_crs(crs)
 
--- src/openlayers/view.py
+++ src/openlayers/view.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 from .abstracts import MyBaseModel
 
 
-class Projection:
+class CRS(object):
+    MERCATOR = "EPSG:3857"
+    EPSG_3857 = "EPSG:3857"
+    WGS_84 = "EPSG:4326"
     EPSG_4326 = "EPSG:4326"
 
 
