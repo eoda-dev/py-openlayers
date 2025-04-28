a88680f32f8b6bf4a2a7bbc991c54002c1bb2f01
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-04-28 17:10:38 +0200

    Update metadata

--- src/openlayers/__init__.py
+++ src/openlayers/__init__.py
@@ -1,7 +1,11 @@
+import importlib.metadata
+
 try:
     from .anywidget import MapWidget
     from .geopandas import *
 except ImportError as e:
     ...
+
+__version__ = importlib.metadata.version(__package__)
 
 __all__ = [MapWidget]
