632ffc9c3a8a4ad4b2b25cd1d9e6fb8f4660c88f
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-05-02 09:25:14 +0200

    Add a future element

---
+++ src/openlayers/__future__/elements.py
@@ -0,0 +1,8 @@
+from ..models.view import View as ViewModel
+
+class View(object):    
+    def __init__(self, center: tuple[float, float], **kwargs) -> None:
+        self._model = ViewModel(**(locals() | kwargs))
+
+    def model_dump(self, **kwargs) -> dict:
+        return self._model.model_dump(**kwargs)
