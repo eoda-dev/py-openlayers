b77ba987893af45f3d980af60d3d7e8bd0150bcf
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-05-04 13:06:41 +0200

    Add more import to main

--- src/openlayers/__init__.py
+++ src/openlayers/__init__.py
@@ -7,10 +7,12 @@ except ImportError as e:
     ...
 
 from . import controls, layers
+from .controls import *
 from .layers import *
 from .map import Map
 from .view import View
+from .styles import FlatStyle
 
 __version__ = importlib.metadata.version(__package__)
 
-__all__ = ["Map", "View", "MapWidget", "controls", "layers"]
+__all__ = ["Map", "View", "MapWidget", "FlatStyle", "controls", "layers"]
