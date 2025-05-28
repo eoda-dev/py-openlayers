585aacf188f96ac5fbd3cf866234bba403a213b8
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-05-28 23:37:28 +0200

    Add drag and drop marimo example

--- marimo/export-notebooks.sh
+++ marimo/export-notebooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 output_folder=${1:-dist}
-files=("getting-started.py" "pmtiles-vector.py")
+files=("getting-started.py" "pmtiles-vector.py" "drag-and-drop.py")
 
 for file in "${files[@]}"; do
   without_extension="${file%.*}"
