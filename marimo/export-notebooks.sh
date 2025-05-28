183be212cd1ac33e81ad22965a25366f9f004fb1
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-05-28 23:00:50 +0200

    Build marimo

--- marimo/export-notebooks.sh
+++ marimo/export-notebooks.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-files=("getting-started.py", "pmtiles-vector.py")
+output_folder=${1:-dist}
+files=("getting-started.py" "pmtiles-vector.py")
 
 for file in "${files[@]}"; do
   without_extension="${file%.*}"
-  marimo export html-wasm "$file" -o dist/"$without_extension".html --mode edit
+  marimo export html-wasm "$file" -o $output_folder/"$without_extension".html --mode edit
 done
 
